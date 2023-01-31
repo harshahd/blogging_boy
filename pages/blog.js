95e6456cbf962e8bb51caf542532da9353c94f81
harshahd <hdharsha4@gmail.com>
2023-01-30 19:34:39 -0800

    Fixed blog component

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,20 @@
+import Head from "next/head"
 
-
-const Blog=() => {
+const blog=() => {
 return (
+    <>
+    <Head>
+<title>My blogs - create and manage all posts</title>
+    </Head>
 <main>
 <h1>My blog</h1>
 <p>Create, Find and manage all your blog post here</p>
 </main>
-
+</>
 )
 
 };
 
-export default Blog;
+
+
+export default blog;
